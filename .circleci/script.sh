339782c4f82da7a90f926e9c8bea3d38b57666a8
iblinov-ua <iblinov-ua@memsql.com>
2020-10-22 18:27:35 +0300

    CI test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -15,7 +15,7 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get update
 DEBIAN_FRONTEND=noninteractive sudo apt-get install --allow-unauthenticated -y --force-yes -m unixodbc-dev odbcinst1debian2 libodbc1 
 
 ##build odbc connector
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL
 # In Travis we are interested in tests with latest C/C version, while for release we must use only latest release tag
 #git submodule update --remote
 cmake --build . --config RelWithDebInfo 
