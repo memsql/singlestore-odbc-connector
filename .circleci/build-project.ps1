bbf2f343f3ba6db695d5c0742cfb4dd368f2f38a
iblinov-ua <iblinov-ua@memsql.com>
2020-11-05 13:48:50 +0200

    CI test

---
+++ .circleci/build-project.ps1
@@ -0,0 +1,9 @@
+if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
+    New-Alias -Name cmake -Value "$Env:ProgramFiles\CMake\bin\cmake.exe"
+}
+
+##build odbc connector
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL
+# In Travis we are interested in tests with latest C/C version, while for release we must use only latest release tag
+#git submodule update --remote
+cmake --build . --config RelWithDebInfo
--- .circleci/build-windows.sh
+++
@@ -1,5 +0,0 @@
-##build odbc connector
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL
-# In Travis we are interested in tests with latest C/C version, while for release we must use only latest release tag
-#git submodule update --remote
-cmake --build . --config RelWithDebInfo
