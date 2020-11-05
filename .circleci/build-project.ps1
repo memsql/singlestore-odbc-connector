6c5cfa07de85306c66476997aa4468063ec8eb88
iblinov-ua <iblinov-ua@memsql.com>
2020-11-05 14:04:49 +0200

    CI test

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -7,4 +7,6 @@ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL
 # In Travis we are interested in tests with latest C/C version, while for release we must use only latest release tag
 #git submodule update --remote
 cmake --build . --config RelWithDebInfo
-msiexec.exe /i wininstall\mariadb-connector-odbc-3.0.6-win32.msi
+cd wininstall
+dir
+#msiexec.exe /i wininstall\mariadb-connector-odbc-3.0.6-win32.msi
