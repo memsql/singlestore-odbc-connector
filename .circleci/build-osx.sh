bbb86a648016a755aaed598149901bf83210ed68
iblinov-ua <iblinov-ua@memsql.com>
2020-11-09 19:15:42 +0200

    CI test

--- .circleci/build-osx.sh
+++ .circleci/build-osx.sh
@@ -22,7 +22,8 @@ export TEST_PASSWORD=
 # install and start MariaDB Server
 # brew install mariadb
 
-ls -la /usr/local/Cellar/openssl/
+ls -la /usr/local/Cellar
+ls -la /usr/local/Cellar/openssl@1.1
 #cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL -DWITH_IODBC=ON -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2o_2 -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2o_2/lib
 #cmake --build . --config RelWithDebInfo
 #
