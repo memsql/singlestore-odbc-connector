90a9ad5401b817b78c93c0bbf9333c47a37e886f
iblinov-ua <iblinov-ua@memsql.com>
2020-11-20 13:23:14 +0200

    Windows CI

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -17,6 +17,8 @@ choco install -y -r --no-progress wixtoolset
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_UNIT_TESTS=Off -DCONC_WITH_MSI=OFF -DWITH_SSL=SCHANNEL .
 cmake --build . --config RelWithDebInfo --parallel 2
 
+cd test
+ctest -V
 #cmake --build . --config RelWithDebInfo
 #dir
 #cd wininstall
