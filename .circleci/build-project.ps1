55fa4bfe183e8da249e0897179aa0e5681f07bdb
iblinov-ua <iblinov-ua@memsql.com>
2020-11-20 13:44:36 +0200

    Windows CI

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -1,5 +1,6 @@
 if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
     New-Alias -Name cmake -Value "$Env:ProgramFiles\CMake\bin\cmake.exe"
+    New-Alias -Name ctest -Value "$Env:ProgramFiles\CMake\bin\ctest.exe"
 }
 
 #mkdir win64
