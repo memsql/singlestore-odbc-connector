7553079617c5a46cf91acf1502d639dcfe234f12
iblinov-ua <iblinov-ua@memsql.com>
2020-11-20 13:18:06 +0200

    Windows CI

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -2,15 +2,18 @@ if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
     New-Alias -Name cmake -Value "$Env:ProgramFiles\CMake\bin\cmake.exe"
 }
 
-mkdir win64
-cd win64
-cmake .. -G "Visual Studio 16 2019 Win64" -DCONC_WITH_MSI=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_SIGNCODE=0 -DWITH_SSL=SCHANNEL -DWITH_OPENSSL=ON
-cd ..
+#mkdir win64
+#cd win64
+#cmake .. -G "Visual Studio 16 2019 Win64" -DCONC_WITH_MSI=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_SIGNCODE=0 -DWITH_SSL=SCHANNEL -DWITH_OPENSSL=ON
+#cd ..
+#
+#cd libmariadb
+#cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo
+#cmake --build . --config RelWithDebInfo --parallel 2
+#cd ..
 
-cd libmariadb
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake --build . --config RelWithDebInfo --parallel 2
-cd ..
+choco install -y -r --no-progress wixtoolset
+
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_UNIT_TESTS=Off -DCONC_WITH_MSI=OFF -DWITH_SSL=SCHANNEL .
 cmake --build . --config RelWithDebInfo --parallel 2
 
