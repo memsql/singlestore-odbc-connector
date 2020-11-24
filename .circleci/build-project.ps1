b4efb02b475418dfcbd0d53b9940cfa126b88ec6
iblinov-ua <iblinov-ua@memsql.com>
2020-11-24 14:28:04 +0200

    Windows CI

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -3,27 +3,13 @@ if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
     New-Alias -Name ctest -Value "$Env:ProgramFiles\CMake\bin\ctest.exe"
 }
 
-#mkdir win64
-#cd win64
-#cmake .. -G "Visual Studio 16 2019 Win64" -DCONC_WITH_MSI=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_SIGNCODE=0 -DWITH_SSL=SCHANNEL -DWITH_OPENSSL=ON
-#cd ..
-#
-#cd libmariadb
-#cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo
-#cmake --build . --config RelWithDebInfo --parallel 2
-#cd ..
-
 choco install -y -r --no-progress wixtoolset
 refreshenv
 
 cmake -A Win32 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_UNIT_TESTS=Off -DCONC_WITH_MSI=OFF -DWITH_SSL=SCHANNEL .
 cmake --build . --config RelWithDebInfo --parallel 2
 
+msiexec.exe /i wininstall\mariadb-connector-odbc-3.1.10-win32.msi
+
 cd test
 ctest -V
-#cmake --build . --config RelWithDebInfo
-#dir
-#cd wininstall
-#cmake --build . --config RelWithDebInfo
-#dir
-#msiexec.exe /i wininstall\mariadb-connector-odbc-3.1.10-win64.msi
