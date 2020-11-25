77d1fa93614b1ba77c9293052da02ec5f853eb1b
iblinov-ua <iblinov-ua@memsql.com>
2020-11-25 14:20:05 +0200

    Windows CI

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -22,12 +22,12 @@ New-ItemProperty -Path $regPath -Name "DATABASE" -Value "test"
 New-ItemProperty -Path $regPath -Name "DESCRIPTION" -Value "MariaDB ODBC test"
 New-ItemProperty -Path $regPath -Name "Driver" -Value "MariaDB ODBC 3.1 Driver"
 New-ItemProperty -Path $regPath -Name "OPTIONS" -Value "0"
-New-ItemProperty -Path $regPath -Name "PORT" -Value "0"
-New-ItemProperty -Path $regPath -Name "PWD" -Value ""
-New-ItemProperty -Path $regPath -Name "SERVER" -Value "localhost"
+New-ItemProperty -Path $regPath -Name "PORT" -Value $ENV:MEMSQL_PORT
+New-ItemProperty -Path $regPath -Name "PWD" -Value $ENV:MEMSQL_PASSWORD
+New-ItemProperty -Path $regPath -Name "SERVER" -Value $ENV:MEMSQL_HOST
 New-ItemProperty -Path $regPath -Name "SSLVERIFY" -Value "0"
 New-ItemProperty -Path $regPath -Name "TCPIP" -Value "1"
-New-ItemProperty -Path $regPath -Name "UID" -Value "root"
+New-ItemProperty -Path $regPath -Name "UID" -Value $ENV:MEMSQL_USER
 New-Item -Path "HKCU:\Software\ODBC\ODBC.INI\ODBC Data Sources"
 New-ItemProperty -Path "HKCU:\Software\ODBC\ODBC.INI\ODBC Data Sources" -Name "maodbc_test" -Value "MariaDB ODBC 3.1 Driver"
 
@@ -37,6 +37,8 @@ Add-OdbcDsn -Name "maodbc_test" -DriverName "MariaDB ODBC 3.1 Driver" -DsnType "
 refreshenv
 
 Add-OdbcDsn -Name "maodbc_test" -DriverName "MariaDB ODBC 3.1 Driver" -DsnType "System" -SetPropertyValue @("Server=localhost", "PORT=3306", "Database=test")
+
+refreshenv
 
 cd test
 ctest -V
