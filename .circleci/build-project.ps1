fc2ff631d772b17f34829f1db5b23598484fd942
iblinov-ua <iblinov-ua@memsql.com>
2020-11-25 12:07:12 +0200

    Windows CI

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -13,10 +13,10 @@ msiexec.exe /i wininstall\mariadb-connector-odbc-3.1.10-win32.msi
 
 New-Item -Path "HKCU:\Software\ODBC"
 New-Item -Path "HKCU:\Software\ODBC\ODBC.INI"
-$regPath = "HKCU:\Software\ODBC\ODBC.INI\test"
+$regPath = "HKCU:\Software\ODBC\ODBC.INI\maodbc_test"
 New-Item -Path $regPath
 New-ItemProperty -Path $regPath -Name "CONN_TIMEOUT" -Value "0"
-New-ItemProperty -Path $regPath -Name "DATABASE" -Value "odbc_test"
+New-ItemProperty -Path $regPath -Name "DATABASE" -Value "test"
 New-ItemProperty -Path $regPath -Name "DESCRIPTION" -Value "MariaDB ODBC test"
 New-ItemProperty -Path $regPath -Name "Driver" -Value "MariaDB ODBC 3.1 Driver"
 New-ItemProperty -Path $regPath -Name "OPTIONS" -Value "0"
@@ -27,7 +27,7 @@ New-ItemProperty -Path $regPath -Name "SSLVERIFY" -Value "0"
 New-ItemProperty -Path $regPath -Name "TCPIP" -Value "1"
 New-ItemProperty -Path $regPath -Name "UID" -Value "root"
 New-Item -Path "HKCU:\Software\ODBC\ODBC.INI\ODBC Data Sources"
-New-ItemProperty -Path "HKCU:\Software\ODBC\ODBC.INI\ODBC Data Sources" -Name "test" -Value "MariaDB ODBC 3.1 Driver"
+New-ItemProperty -Path "HKCU:\Software\ODBC\ODBC.INI\ODBC Data Sources" -Name "maodbc_test" -Value "MariaDB ODBC 3.1 Driver"
 
 cd test
 ctest -V
