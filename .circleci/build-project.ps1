5681d98de4983c472862a3e068f67f5addf8fa35
iblinov-ua <iblinov-ua@memsql.com>
2020-11-25 14:58:33 +0200

    Windows CI

--- .circleci/build-project.ps1
+++ .circleci/build-project.ps1
@@ -22,11 +22,15 @@ New-ItemProperty -Path $regPath -Name "DESCRIPTION" -Value "MariaDB ODBC test"
 New-ItemProperty -Path $regPath -Name "Driver" -Value "MariaDB ODBC 3.1 Driver"
 New-ItemProperty -Path $regPath -Name "OPTIONS" -Value "0"
 New-ItemProperty -Path $regPath -Name "PORT" -Value $ENV:MEMSQL_PORT
+New-ItemProperty -Path $regPath -Name "TEST_PORT" -Value $ENV:MEMSQL_PORT
 New-ItemProperty -Path $regPath -Name "PWD" -Value $ENV:MEMSQL_PASSWORD
+New-ItemProperty -Path $regPath -Name "TEST_PASSWORD" -Value $ENV:MEMSQL_PASSWORD
 New-ItemProperty -Path $regPath -Name "SERVER" -Value $ENV:MEMSQL_HOST
+New-ItemProperty -Path $regPath -Name "TEST_SERVER" -Value $ENV:MEMSQL_HOST
 New-ItemProperty -Path $regPath -Name "SSLVERIFY" -Value "0"
 New-ItemProperty -Path $regPath -Name "TCPIP" -Value "1"
 New-ItemProperty -Path $regPath -Name "UID" -Value $ENV:MEMSQL_USER
+New-ItemProperty -Path $regPath -Name "TEST_UID" -Value $ENV:MEMSQL_USER
 New-Item -Path "HKCU:\Software\ODBC\ODBC.INI\ODBC Data Sources"
 New-ItemProperty -Path "HKCU:\Software\ODBC\ODBC.INI\ODBC Data Sources" -Name "maodbc_test" -Value "MariaDB ODBC 3.1 Driver"
 
