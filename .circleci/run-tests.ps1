326a7a0cc83ddf01d9b5e6b243cddffa0eafaf68
iblinov-ua <iblinov-ua@memsql.com>
2020-11-26 15:43:21 +0200

    Windows CI

--- .circleci/run-tests.ps1
+++ .circleci/run-tests.ps1
@@ -2,5 +2,11 @@ if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
     New-Alias -Name ctest -Value "$Env:ProgramFiles\CMake\bin\ctest.exe"
 }
 
+$env:TEST_SCHEMA="test"
+$env:TEST_PORT=$ENV:MEMSQL_PORT
+$env:TEST_PASSWORD=$ENV:MEMSQL_PASSWORD
+$env:TEST_SERVER=$ENV:MEMSQL_HOST
+$env:TEST_UID=$ENV:MEMSQL_USER
+
 cd test
-ctest -V -u $ENV:MEMSQL_USER -p $ENV:MEMSQL_PASSWORD -P $ENV:MEMSQL_PORT -S $ENV:MEMSQL_HOST -s "test"
+ctest -V
