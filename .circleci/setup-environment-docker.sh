c66a13c657fd55487bfeb7e3ad4ee7b89499b419
iblinov-ua <iblinov-ua@memsql.com>
2021-01-05 19:50:50 +0200

    test

---
+++ .circleci/setup-environment-docker.sh
@@ -0,0 +1,19 @@
+if [[ $IMAGE_NAME == centos* ]]
+then
+  yum update
+  yum -y install curl
+  yum -y install mysql
+  yum -y install git
+  yum -y install gcc gcc-c++
+  yum -y install make cmake
+  yum -y install unixODBC unixODBC-devel
+else
+  apt update
+  apt install -y curl
+  apt-get update
+  apt-get install -y mysql-client-core-5.5
+  apt-get install -y unixodbc odbcinst
+  apt-get install -y unixodbc-dev
+  apt-get install --allow-unauthenticated -y --force-yes -m unixodbc-dev odbcinst1debian2 libodbc1
+  apt-get clean
+fi
