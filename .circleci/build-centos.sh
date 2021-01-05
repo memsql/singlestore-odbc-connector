f1f5704a859e2992ce78d4690086d996d3e9ce83
iblinov-ua <iblinov-ua@memsql.com>
2021-01-05 18:44:02 +0200

    test

--- .circleci/build-centos.sh
+++ .circleci/build-centos.sh
@@ -6,17 +6,7 @@ set -eo pipefail
 # test different type of configuration
 ###################################################################################################################
 
-echo 127.0.0.1 singlestore.test.com | sudo tee -a /etc/hosts
 export PROJ_PATH=`pwd`
-mkdir tmp
-.circleci/gen-ssl.sh singlestore.test.com tmp
-export SSLCERT=$PROJ_PATH/tmp
-
-# list ssl certificates
-ls -lrt ${SSLCERT}
-
-DEBIAN_FRONTEND=noninteractive sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install --allow-unauthenticated -y --force-yes -m unixodbc-dev odbcinst1debian2 libodbc1 
 
 ## build odbc connector
 cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL
