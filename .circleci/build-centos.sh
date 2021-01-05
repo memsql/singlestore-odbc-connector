3be185511cd2e29e4990ef1706ee41bd64985f1e
iblinov-ua <iblinov-ua@memsql.com>
2021-01-05 19:13:55 +0200

    test

--- .circleci/build-centos.sh
+++ .circleci/build-centos.sh
@@ -9,7 +9,7 @@ set -eo pipefail
 export PROJ_PATH=`pwd`
 
 ## build odbc connector
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DWITH_OPENSSL=Off
 cmake --build . --config ${BUILD_TYPE}
 
 ## Export password
