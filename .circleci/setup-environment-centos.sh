23efc738f7caa549a0df4138d799bde338d1b3d0
iblinov-ua <iblinov-ua@memsql.com>
2021-01-05 19:29:53 +0200

    test

---
+++ .circleci/setup-environment-centos.sh
@@ -0,0 +1,7 @@
+yum update
+yum -y install curl
+yum -y install mysql
+yum -y install git
+yum -y install gcc gcc-c++
+yum -y install make cmake
+yum -y install unixODBC unixODBC-devel
---
+++ .circleci/setup-environment-debian.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y curl
+sudo apt-get update
+sudo apt-get install -y mysql-client-core-5.5
+sudo apt-get install unixodbc odbcinst
+sudo apt-get install unixodbc-dev
+sudo apt-get install --allow-unauthenticated -y --force-yes -m unixodbc-dev odbcinst1debian2 libodbc1
+sudo apt-get clean
