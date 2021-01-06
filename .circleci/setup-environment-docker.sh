5710fde8327353b6ef084fdc24372ddc96756406
iblinov-ua <iblinov-ua@memsql.com>
2021-01-06 12:16:20 +0200

    test

--- .circleci/setup-environment-docker.sh
+++ .circleci/setup-environment-docker.sh
@@ -15,7 +15,7 @@ else
   apt-get install -y unixodbc odbcinst
   apt-get install -y unixodbc-dev
   apt-get install -y build-essential
-  apt-get install -y make
+  apt-get install -y cmake
   apt-get install --allow-unauthenticated -y --force-yes -m unixodbc-dev odbcinst1debian2 libodbc1
   apt-get clean
 fi
