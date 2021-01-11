458ac7d0f65cb82bd599b713d87acf55bb4f7eea
iblinov-ua <iblinov-ua@memsql.com>
2021-01-11 15:30:07 +0200

    test

--- .circleci/build-docker.sh
+++ .circleci/build-docker.sh
@@ -9,7 +9,7 @@ set -eo pipefail
 export PROJ_PATH=`pwd`
 
 ## build odbc connector
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DWITH_OPENSSL=Off
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DWITH_SSL=OPENSSL
 cmake --build . --config ${BUILD_TYPE}
 
 ## Export password
