9aaf0d4937523344bf0bceda0526865372c9e535
iblinov-ua <iblinov-ua@memsql.com>
2021-01-11 16:50:59 +0200

    Fixed setup environment for Centos and Debian

--- .circleci/setup-environment-docker.sh
+++ .circleci/setup-environment-docker.sh
@@ -5,15 +5,4 @@ then
 else
   apt-get update
   apt-get install -y cmake make gcc libssl-dev unixodbc-dev
-#  apt update
-#  apt install -y curl
-#  apt-get update
-#  apt-get install -y mysql-client-core-5.5
-#  apt-get install -y unixodbc odbcinst
-#  apt-get install -y unixodbc-dev
-#  apt-get install -y build-essential
-#  apt-get install -y cmake
-#  apt-get install -y libssl-dev
-#  apt-get install --allow-unauthenticated -y --force-yes -m unixodbc-dev odbcinst1debian2 libodbc1
-#  apt-get clean
 fi
