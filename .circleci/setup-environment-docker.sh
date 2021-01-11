1f28408957350535175e1fe52deb17ad1c1c0efa
iblinov-ua <iblinov-ua@memsql.com>
2021-01-11 15:01:56 +0200

    Resolved comments

--- .circleci/setup-environment-docker.sh
+++ .circleci/setup-environment-docker.sh
@@ -3,7 +3,6 @@ then
   yum update
   yum -y install curl
   yum -y install mysql
-  yum -y install git
   yum -y install gcc gcc-c++
   yum -y install make cmake
   yum -y install unixODBC unixODBC-devel
