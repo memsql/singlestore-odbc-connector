f7b866642de9f5c4c74ffda3a15d8c388e62e4b3
iblinov-ua <iblinov-ua@memsql.com>
2021-01-13 17:37:42 +0200

    Removed git from centos env

--- .circleci/setup-environment-docker.sh
+++ .circleci/setup-environment-docker.sh
@@ -1,7 +1,7 @@
 if [[ $IMAGE_NAME == centos* ]]
 then
   yum update
-  yum -y install git cmake make gcc openssl-devel unixODBC unixODBC-devel
+  yum -y install cmake make gcc openssl-devel unixODBC unixODBC-devel
 else
   apt-get update
   apt-get install -y cmake make gcc libssl-dev unixodbc-dev
