adffb551f4112054578bee8da81df8410a1e03e3
iblinov-ua <iblinov-ua@memsql.com>
2021-02-12 16:05:38 +0200

    Fix macos build

--- .circleci/build-osx.sh
+++ .circleci/build-osx.sh
@@ -3,7 +3,6 @@
 set -eo pipefail
 
 # set variables for Connector/ODBC
-#export TEST_DRIVER="$PWD/libmaodbc.dylib"
 export OPENSSL_ROOT_DIR=(/usr/local/Cellar/openssl@1.1/1.1.1*)
 
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OPENSSL=ON -DWITH_SSL=OPENSSL -DWITH_IODBC=ON -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR} -DOPENSSL_LIBRARIES=${OPENSSL_ROOT_DIR}/lib
