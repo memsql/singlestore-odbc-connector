863945ea67ec45171051d53046df8a7121c7504a
iblinov-ua <iblinov-ua@memsql.com>
2021-02-12 12:33:51 +0200

    CI test

--- test/tap.h
+++ test/tap.h
@@ -953,7 +953,7 @@ SQLHANDLE DoConnect(SQLHANDLE Connection, BOOL DoWConnect,
         if (SQL_SUCCEEDED(rc))
         {
             /* ANSI driver file name contains 5a.{dll|so} */
-            unicode_driver= strstr((char*)driver_name, "a.") == NULL ? 1 : 0;
+            unicode_driver= strstr((char*)driver_name, "a.") == NULL ? ANSI_DRIVER : UNICODE_DRIVER;
         }
     }
 
