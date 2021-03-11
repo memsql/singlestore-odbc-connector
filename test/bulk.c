02e2edd0716daa1120cafea6c1ac5d3582fdf844
iblinov-ua <iblinov-ua@memsql.com>
2021-03-11 17:00:16 +0200

    commented failed tests

--- test/bulk.c
+++ test/bulk.c
@@ -573,6 +573,10 @@ ODBC_TEST(t_odbc149)
   SQLLEN  cInd[]= {SQL_NTS, SQL_NTS, SQL_NTS}, wLen; 
   SQLWCHAR w[][8]= {{'x', 'x', 'x', 0}, {'y', 'y', 0}, {'z', 'z', 'z', 'a', 0}}, wBuf[16];
 
+  if (iOdbc() && is_unicode_driver()) {
+      return OK;
+  }
+
   OK_SIMPLE_STMT(Stmt, "DROP TABLE IF EXISTS odbc149");
   OK_SIMPLE_STMT(Stmt, "CREATE TABLE odbc149 (id int not null, ts timestamp, c varchar(16), b tinyblob, w varchar(16))");
 
