3fbd722413b0195ae5fec79e39f2f5b4d1b3ef20
iblinov-ua <iblinov-ua@memsql.com>
2021-04-08 14:13:54 +0300

    revert unnecessary changes

--- test/unicode.c
+++ test/unicode.c
@@ -1066,12 +1066,10 @@ ODBC_TEST(t_bug34672)
                                   SQL_WCHAR, 0, 0, chars,
                                   inchars * sizeof(SQLWCHAR), NULL));
 
-  // CSPS Error: Failed to convert a wchar parameter
   CHECK_STMT_RC(Stmt1, SQLExecDirectW(Stmt1, CW("select ? FROM DUAL"), SQL_NTS));
   CHECK_STMT_RC(Stmt1, SQLFetch(Stmt1));
   CHECK_STMT_RC(Stmt1, SQLGetData(Stmt1, 1, SQL_C_WCHAR, result,
                             sizeof(result), &reslen));
-  // SSPS Error: result[iOdbc() ? 1 : 2](65533)!=0(0)
   is_num(result[iOdbc() ? 1 : 2], 0);
   for (i= 0; i < inchars; ++i)
   {
