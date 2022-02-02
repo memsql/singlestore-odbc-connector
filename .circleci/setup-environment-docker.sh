93742874dd4507756c65157b84fe13ddda114c26
Pavlo <pmishchenko-ua@memsql.com>
2022-02-02 15:56:07 +0200

    Fix Centos 8 CI job
    Summary: Centos 8 EOL likely causes this.
    
    Test Plan: CircleCI https://app.circleci.com/pipelines/github/memsql/singlestore-odbc-connector/2080/workflows/4530aa9d-adb8-40ff-95aa-39bc40155509/jobs/11982
    
    Reviewers: amakarovych-ua
    
    Reviewed By: amakarovych-ua
    
    Subscribers: engineering-list
    
    JIRA Issues: PLAT-6058
    
    Differential Revision: https://grizzly.internal.memcompute.com/D54576

--- .circleci/setup-environment-docker.sh
+++ .circleci/setup-environment-docker.sh
@@ -19,7 +19,9 @@
 
 if [[ $IMAGE_NAME == centos* ]]
 then
-  yum update
+  sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
+  sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+  yum -y update
   yum -y install gcc-c++ make gcc openssl-devel unixODBC unixODBC-devel wget bind-utils
   wget https://github.com/Kitware/CMake/releases/download/v3.20.3/cmake-3.20.3-linux-x86_64.tar.gz
   cd /usr || exit
