d15f1b8d3185ae4191d63dba1540092982c20527
Pavlo <pmishchenko-ua@memsql.com>
2022-09-16 13:11:15 +0300

    Force debugging

--- ma_debug.h
+++ ma_debug.h
@@ -29,7 +29,8 @@ void ma_debug_print(my_bool ident, char *format, ...);
 void ma_debug_print_error(MADB_Error *err);
 
 /* Debug is on for connection */
-#define MDBUG_C_IS_ON(C) ((C) && (((MADB_Dbc*)(C))->Options & MA_DEBUG_FLAG))
+// #define MDBUG_C_IS_ON(C) ((C) && (((MADB_Dbc*)(C))->Options & MA_DEBUG_FLAG))
+#define MDBUG_C_IS_ON(C) ((C))
 
 #ifdef WIN32
 #define MDBUG_C_ENTER(C,A)\
