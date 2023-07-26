35e9b85fdc895015f8be03455210d0c1b551cc62
Adalbert Makarovych <amakarovych0ua@singlestore.com>
2023-07-26 22:38:59 +0300

    Added debug output

--- ma_platform_win32.c
+++ ma_platform_win32.c
@@ -142,7 +142,7 @@ char *MADB_ConvertFromLatin1Char(const char *Str, SQLINTEGER StrCharLen, SQLULEN
   
   if (Error)
     *Error= 0;
-
+  printf("AAAA1 %d\n", StrCharLen);
   if (StrCharLen == SQL_NTS) 
   {
     StrActualCharLen = strlen(Str) + 1;
@@ -150,6 +150,7 @@ char *MADB_ConvertFromLatin1Char(const char *Str, SQLINTEGER StrCharLen, SQLULEN
   {
     StrActualCharLen = StrCharLen;
   }
+  printf("AAAA2 %d\n", StrActualCharLen);
 
   if (!(AscStr = (char *)MADB_CALLOC(StrActualCharLen * 2)))
     return NULL;
@@ -157,6 +158,7 @@ char *MADB_ConvertFromLatin1Char(const char *Str, SQLINTEGER StrCharLen, SQLULEN
   for (i = 0; i < StrActualCharLen; i++) 
   {
     char c = *(Str + i);
+    printf("AAAA3 %c\n", c);
     if (c & 0x80) 
     {
       *(AscStr + AscLen++) = c;
@@ -165,10 +167,12 @@ char *MADB_ConvertFromLatin1Char(const char *Str, SQLINTEGER StrCharLen, SQLULEN
       *(AscStr + AscLen++) = 0xc0 | (c >> 6);
       *(AscStr + AscLen++) = 0x80 | (c & 0x3f);
     }
+    printf("AAAA4 %d\n", AscLen);
   }
 
   if (Length)
     *Length= (SQLINTEGER)AscLen;
+  printf("AAAA5 %d\n", *Length);
   return AscStr;
 }
 /* }}} */
