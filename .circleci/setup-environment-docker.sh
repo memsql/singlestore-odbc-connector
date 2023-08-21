aa4d5b974a418fce63a538c5a92ceb8243d2207e
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2023-08-21 15:11:56 +0300

    Fixed tests
    Summary:
    Increased timeout for cursor test.
    Changed to install packages after adding a repo.
    
    Test Plan:
    https://app.circleci.com/pipelines/github/memsql/singlestore-odbc-connector/2919
    
    check point
    
    Installed old version of the urllib3 as newest requires openssl 2 which is not supported by centos 7
    
    check point
    
    Reviewers: pmishchenko-ua
    
    Reviewed By: pmishchenko-ua
    
    Subscribers: engineering-list
    
    Differential Revision: https://grizzly.internal.memcompute.com/D64389

--- .circleci/setup-environment-docker.sh
+++ .circleci/setup-environment-docker.sh
@@ -40,14 +40,14 @@ then
   make altinstall
   ln -sfn /usr/local/bin/python3.7 /usr/bin/python3
   ln -sfn /usr/local/bin/pip3.7 /usr/bin/pip3
+  pip3 install urllib3==1.26.16
 else
   apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 648ACFD622F3D138  # Debian 10/buster archive signing key
   apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0E98404D386FA1D9  # Debian 11/bullseye archive signing key
   apt-key adv --recv-keys --keyserver keyserver.ubuntu.com DCC9EFBF77E11517  # Debian 10/buster release key
-  apt-get update
-  apt-get install -y cmake make gcc libssl-dev dnsutils
   echo "deb http://deb.debian.org/debian buster main contrib non-free" >> /etc/apt/sources.list.d/tutu.list
   apt-get update
+  apt-get install -y cmake make gcc libssl-dev dnsutils
   apt-get install -y unixodbc-dev 'libglib2.0-0' libglib2.0-dev libsecret-1-dev xdg-utils w3m curl
   # install python and pip
   apt-get install -y python3 python3-pip
