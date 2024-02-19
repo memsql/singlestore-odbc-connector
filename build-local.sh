cafc9c3d3e199d1712a3c5f54dfd82c48eed7d14
Pavlo Mishchenko <pmishchenko-ua@singlestore.com>
2024-02-19 09:00:03 +0200

    Bump version to 1.1.5
    Summary: Bump version for release
    
    Test Plan: n/a
    
    Reviewers: amakarovych-ua
    
    Reviewed By: amakarovych-ua
    
    Subscribers: engineering-list
    
    Differential Revision: https://grizzly.internal.memcompute.com/D66872

--- build-local.sh
+++ build-local.sh
@@ -1,3 +1,5 @@
+set -eu pipefail
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=DEBUG -DCONC_WITH_UNIT_TESTS=Off -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_SSL=OPENSSL
-cmake --build . --config DEBUG
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_UNIT_TESTS=Off -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_SSL=OPENSSL
+cmake --build . --config RelWithDebInfo
+cd ..
